6c2e19cf335777d6b89eca2526cc5415268ad051
Cristnunezj <141673137+Cristnunezj@users.noreply.github.com>
2023-10-23 15:57:31 -0600

    ResultadoAP

---
+++ Models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql'); // Mueve la importación del módulo mysql aquí
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.static('public'));
+  }
+
+  routes() {
+    this.app.get('/bienvenido', (req, res) => {
+      res.send("<h1>Bienvenido al server de Cristhian</h1>")
+    });
+    this.app.get('/cons', (req, res) => {
+      res.send("Esta es una prueba")
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("http://127.0.0.1:" + this.port)
+    });
+  }
+}
+
+module.exports = Server;
